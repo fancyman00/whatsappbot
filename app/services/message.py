db111cb87f7dfec8bb825a01aedcbb58fa033e14
fancyman00 <aleop00040005@gmail.com>
2024-10-26 15:58:15 +0300

    delayed message fix

--- app/services/message.py
+++ app/services/message.py
@@ -1,11 +1,9 @@
-import asyncio
 import datetime
 
 from fastapi import Depends
 from sqlalchemy import func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.config import settings
 from app.models.message import Messages
 from app.services.database import get_session
 from app.services.green_api import ChatAPI, get_chat_api
@@ -20,20 +18,16 @@ class MessageService:
         self.chat_api = chat_api
 
     async def delayed_message(self, start_time: datetime, sender: str):
-        await asyncio.sleep(settings.openai_setting.timeout)
-        last_message = await Messages.get_last_message(sender)
+        last_message = await self.get_last_message(sender)
 
-        start_time = start_time.replace(tzinfo=datetime.timezone.utc)
-        last_message = last_message.created_at.replace(tzinfo=datetime.timezone.utc)
-
-        if last_message >= start_time + datetime.timedelta(minutes=5):
+        if last_message.created_at >= start_time + datetime.timedelta(minutes=5):
             return None
 
         memories = await self.get_messages(sender)
 
         promt = "I asked you a question above, but is there anything else you can say about this? Start writing something like this: I would like to add one more thing"
         response_text = await self.open_ai.generate_message(promt, memories, False)
-        await self.green_api.send_message(sender, response_text)
+        await self.chat_api.send_message(sender, response_text)
 
     async def get_messages(self, sender: str):
         query = (
@@ -45,7 +39,7 @@ class MessageService:
         _result = await self.session.execute(query)
         return _result.scalars().all()
 
-    async def get_last_message(self, sender: str):
+    async def get_last_message(self, sender: str) -> Messages | None:
         query = (
             select(Messages)
             .where(Messages.sender == sender)
@@ -53,7 +47,7 @@ class MessageService:
             .limit(1)
         )
         _result = await self.session.execute(query)
-        return _result.scalars().all()
+        return _result.scalars().first()
 
     async def get_number_of_message(self, sender):
         query = select(func.count(Messages.id)).where(Messages.sender == sender)
